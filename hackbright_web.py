ba936640b86bf970c5a5fab868ae6d98d0e37021
Your Name <you@example.com>
2020-01-29 17:47:20 -0800

    things happened

---
+++ hackbright_web.py
@@ -0,0 +1,64 @@
+"""A web application for tracking projects, students, and student grades."""
+
+from flask import Flask, request, render_template
+
+import hackbright
+
+app = Flask(__name__)
+
+
+@app.route("/student")
+def get_student():
+    """Show information about a student."""
+
+    # github = "jhacks"
+    github = request.args.get('github')
+
+    first, last, github = hackbright.get_student_by_github(github)
+
+    html = render_template('student_info.html',
+                            first=first,
+                            last=last,
+                            github=github)
+    return html 
+
+@app.route('/student-search') 
+def get_student_form():
+    """ Show form for searching for a student. """
+
+    return render_template('student_search.html')
+
+
+@app.route('/student-add', methods=['POST'])
+def student_add():
+    """Add a student."""
+    first_name = request.form.get('fname')
+    print(first_name)
+
+    last_name = request.form.get('lname')
+    print(last_name)
+
+    github = request.form.get('gitname')
+
+    # QUERY = """
+    #     INSERT INTO students (first_name, last_name, github)
+    #       VALUES (:fname, :lname, :gitname)
+    #     """
+
+    # hackbright.db.session.execute(QUERY, {'fname': first_name,
+    #                            'lname': last_name,
+    #                            'gitname': github})
+
+    # hackbright.db.session.commit()
+
+    hackbright.make_new_student(first_name, last_name, github)
+
+   # print(f"Successfully added student: {first_name} {last_name} {github}")
+
+    return render_template('add_student.html', fname=first_name,
+                            lname=last_name, gitname=github)
+
+
+if __name__ == "__main__":
+    hackbright.connect_to_db(app)
+    app.run(debug=True, host="0.0.0.0")
