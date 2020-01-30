9ea26450f2522017650d2f16fa8cc743fba86222
Your Name <you@example.com>
2020-01-30 13:03:32 -0800

    Fixed changes, working on display grades

--- hackbright_web.py
+++ hackbright_web.py
@@ -16,10 +16,13 @@ def get_student():
 
     first, last, github = hackbright.get_student_by_github(github)
 
+    student_grade = hackbright.get_grades_by_github(github)
+
     html = render_template('student_info.html',
                             first=first,
                             last=last,
-                            github=github)
+                            github=github,
+                            grade=student_grade)
     return html 
 
 @app.route('/student-search') 
@@ -29,9 +32,41 @@ def get_student_form():
     return render_template('student_search.html')
 
 
-@app.route('/student-add', methods=['POST'])
+@app.route('/student-add')
 def student_add():
     """Add a student."""
+    
+    # first_name = request.form.get('fname')
+    # print(first_name)
+
+    # last_name = request.form.get('lname')
+    # print(last_name)
+
+    # github = request.form.get('gitname')
+    # # QUERY = """
+    # #     INSERT INTO students (first_name, last_name, github)
+    # #       VALUES (:fname, :lname, :gitname)
+    # #     """
+
+    # # hackbright.db.session.execute(QUERY, {'fname': first_name,
+    # #                            'lname': last_name,
+    # #                            'gitname': github})
+
+    # # hackbright.db.session.commit()
+
+    # hackbright.make_new_student(first_name, last_name, github)
+
+    # print(f"Successfully added student: {first_name} {last_name} {github}")
+
+    return render_template('add_student.html')
+        # fname=first_name,
+        #                    lname=last_name, gitname=github)
+
+
+@app.route('/student-display', methods=['POST'])
+def student_display():
+    """Display success message and link to student account."""
+
     first_name = request.form.get('fname')
     print(first_name)
 
@@ -39,7 +74,6 @@ def student_add():
     print(last_name)
 
     github = request.form.get('gitname')
-
     # QUERY = """
     #     INSERT INTO students (first_name, last_name, github)
     #       VALUES (:fname, :lname, :gitname)
@@ -53,11 +87,11 @@ def student_add():
 
     hackbright.make_new_student(first_name, last_name, github)
 
-   # print(f"Successfully added student: {first_name} {last_name} {github}")
-
-    return render_template('add_student.html', fname=first_name,
-                            lname=last_name, gitname=github)
-
+    print(f"Successfully added student: {first_name} {last_name} {github}")
+      
+    return render_template('success_student.html', first=first_name, last=last_name,
+                            github=github)
+        
 
 if __name__ == "__main__":
     hackbright.connect_to_db(app)
